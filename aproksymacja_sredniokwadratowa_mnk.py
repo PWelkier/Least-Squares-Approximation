8d95eb04ab1339c9b055eac6d405a48fc9462365
PWelkier <149411241+PWelkier@users.noreply.github.com>
2024-05-08 21:32:05 +0200

    Update aproksymacja_sredniokwadratowa_mnk.py

--- aproksymacja_sredniokwadratowa_mnk.py
+++ aproksymacja_sredniokwadratowa_mnk.py
@@ -1,40 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def matrix_multiply(A, B):
-    result = np.zeros((A.shape[0], B.shape[1]))
-    for i in range(A.shape[0]):
-        for j in range(B.shape[1]):
-            for k in range(A.shape[1]):
-                result[i][j] += A[i][k] * B[k][j]
-    return result
-
-
-def matrix_transpose(A):
-    result = np.zeros((A.shape[1], A.shape[0]))
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            result[j][i] = A[i][j]
-    return result
-
-
-def cholesky_decomposition(A):
-    n = len(A)
+def rozkladCholesky(M):
+    n = len(M)
     L = np.zeros((n, n))
 
     for i in range(n):
         for j in range(i + 1):
             s = sum(L[i][k] * L[j][k] for k in range(j))
             if i == j:
-                L[i][j] = np.sqrt(A[i][i] - s)
+                L[i][j] = np.sqrt(M[i][i] - s)
             else:
-                L[i][j] = (1.0 / L[j][j] * (A[i][j] - s))
+                L[i][j] = (1.0 / L[j][j] * (M[i][j] - s))
     return L
 
 
-def solve_linear_equations(A, b):
-    L = cholesky_decomposition(A)
+def rozwiazanieUkladuRownan(M, b):
+    L = rozkladCholesky(M)
     y = np.zeros(len(b))
     x = np.zeros(len(b))
     for i in range(len(b)):
@@ -46,32 +28,31 @@ def solve_linear_equations(A, b):
 
 
 def Welkier_Patryk_Aproks_MNK(x, y, n):
-
-    A = np.zeros((len(x), n + 1))
+    M = np.zeros((len(x), n + 1))
     for i in range(n + 1):
         for j in range(len(x)):
-            A[j][i] = x[j] ** i
+            M[j][i] = x[j] ** i
 
-    A_transpose = matrix_transpose(A)
-    A_transpose_A = matrix_multiply(A_transpose, A)
-    A_transpose_y = np.dot(A_transpose, y)
+    M_t = np.transpose(M)
+    M_t_M = np.dot(M_t, M)
+    M_t_y = np.dot(M_t, y)
 
-    coefficients = solve_linear_equations(A_transpose_A, A_transpose_y)
+    wspolczynniki = rozwiazanieUkladuRownan(M_t_M, M_t_y)
 
     plt.scatter(x, y, label='Punkty')
     xp = np.linspace(min(x), max(x), 100)
     yp = np.zeros_like(xp)
     for i in range(n + 1):
-        yp += coefficients[i] * xp ** i
-    plt.plot(xp, yp, label='Wielomian aproksymacyjny', color='red')
+        yp += wspolczynniki[i] * xp ** i
+    plt.plot(xp, yp, label='Wielomian aproksymacyjny', color='black')
     plt.legend()
     plt.show()
 
-    return coefficients
+    return wspolczynniki
 
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([1, 3, 5, 7, 9])
 n = 3
-coefficients = Welkier_Patryk_Aproks_MNK(x, y, n)
-print("Współczynniki wielomianu aproksymacyjnego:", coefficients)
+wspolczynniki = Welkier_Patryk_Aproks_MNK(x, y, n)
+print("Współczynniki wielomianu aproksymacyjnego:", wspolczynniki)
